cc48a96bee3848fa0049d2a49a64939162e5535b
Andrii4545 <goldwin454545@gmail.com>
2021-12-11 23:31:20 +0300

    media

--- js/main.js
+++ js/main.js
@@ -1,33 +1,19 @@
 window.onload = function(){
-    console.log(124214)
-
     
     var sing = document.getElementById("sing");
     var wsing = document.getElementById("a2");
     var join = document.getElementById("join");
     var wjoin = document.getElementById("a1");
-        console.log(wsing)
 
-
-    join.onclick = function () {
-        
-        console.log(2+2)
+    join.onclick = function () { 
         wjoin.classList.add("active")
         wjoin.classList.remove("noactive")
-        wsing.classList.remove("active")
-        
-        
-        
-      
+        wsing.classList.remove("active")            
     };
-    sing.onclick = function () {
-        
-        console.log(1+2)
+    sing.onclick = function () {        
         wjoin.classList.remove("active")
         wjoin.classList.add("noactive")
-        wsing.classList.add("active")
-        
-       
+        wsing.classList.add("active")      
     };
     
 }
